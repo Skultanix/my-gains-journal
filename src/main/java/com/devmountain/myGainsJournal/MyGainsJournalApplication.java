b5dcebec6cdabe6a0c5a320a591ed357e154ac9b
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-05-09 14:42:48 -0600

    Initial database build out and spring project setup complete.

---
+++ src/main/java/com/devmountain/myGainsJournal/MyGainsJournalApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.myGainsJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGainsJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGainsJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmountain/myGainsJournal/MyGainsJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmountain.myGainsJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGainsJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
